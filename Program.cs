28eb64957f9f7509cb53b7e487f5bf6b0f8b925d
hape04001 <92376988+hape04001@users.noreply.github.com>
2021-10-12 15:44:28 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Number_Gueser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Random  num = new Random();
+            int tal = num.Next(1, 101);
+            Console.WriteLine("gissa tal mellan 0 och 100");
+            int försök = 0;
+            while(true)
+            {
+               
+                int awnser = int.Parse(Console.ReadLine()); 
+                
+                 
+                if(awnser < tal){
+                    Console.WriteLine("talet för lågt. Försök igen");
+                    försök++;
+                }
+                else if (awnser > tal){
+                    Console.WriteLine("talet för högt. försök igen");
+                    försök++;
+                }
+                else{
+                    försök++;
+                    Console.WriteLine($"Rätt tal. Du gissade rätt efter {försök} försök");
+                    break;
+                }
+
+            }
+
+        }
+    }
+}
